844108a9e9dc35f1fbb2dcdef62bed8132ff25b2
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2021-06-26 09:39:22 -0400

    exam

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import perfil from '../images/perfil.jpg';
+const Content = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import lin from "../images/lin.svg";
+import ins from "../images/ins.svg"
+const Footer = () => {
+    return (
+        <div class="card-footer">
+            <div class="footer-item">
+                <p class="status">  <span><img src={lin} alt="" /></span> Yaretas24</p>
+            </div>
+            <div class="footer-item">
+                <p class="status">  <span><img src={ins} alt="" /></span> Yaretas24</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
